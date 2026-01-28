abeef44d1f78fff7e444c338d9d8542bf6e5f6d2
Beeeq <vanbuurenelias@gmail.com>
2026-01-28 15:13:50 +0100

    add rarity to mask

--- Global game jam 26/Assets/Scripts/Mask.cs
+++ Global game jam 26/Assets/Scripts/Mask.cs
@@ -1,10 +1,19 @@
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Mask", menuName = "Scriptable Objects/Mask")]
+
+
 public class Mask : ScriptableObject
 {
+    public Rarity rarity;
     public string maskName;
     public string description;
     public Sprite sprite;
     public int id;
+}
+public enum Rarity
+{
+    Common,
+    Uncommon,
+    Special
 }
